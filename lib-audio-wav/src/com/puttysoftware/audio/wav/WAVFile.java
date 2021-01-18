c146ca705799072435a00d830e08c892c39a04df
Eric Ahnell <eric.ahnell@puttysoftware.com>
2021-01-18 08:09:58 -0500

    Fix resource leak.

--- lib-audio-wav/src/com/puttysoftware/audio/wav/WAVFile.java
+++ lib-audio-wav/src/com/puttysoftware/audio/wav/WAVFile.java
@@ -8,61 +8,72 @@ import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
+import javax.sound.sampled.Line;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 class WAVFile extends WAVPlayer {
-	private final String filename;
+    private final String filename;
 
-	public WAVFile(final String wavfile) {
-		super();
-		this.filename = wavfile;
-	}
+    public WAVFile(final String wavfile) {
+        super();
+        this.filename = wavfile;
+    }
 
-	@Override
-	public void play() {
-		if (this.filename != null) {
-			final File soundFile = new File(this.filename);
-			if (soundFile.exists()) {
-				try (FileInputStream inputStream = new FileInputStream(soundFile)) {
-					new Thread() {
-						@Override
-						public void run() {
-							try (AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(inputStream)) {
-								final AudioFormat format = audioInputStream.getFormat();
-								final DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
-								try (SourceDataLine auline = (SourceDataLine) AudioSystem.getLine(info)) {
-									auline.open(format);
-									auline.start();
-									int nBytesRead = 0;
-									final byte[] abData = new byte[WAVPlayer.EXTERNAL_BUFFER_SIZE];
-									try {
-										while (nBytesRead != -1) {
-											nBytesRead = audioInputStream.read(abData, 0, abData.length);
-											if (nBytesRead >= 0) {
-												auline.write(abData, 0, nBytesRead);
-											}
-										}
-									} catch (final IOException e) {
-										throw new WAVException(e);
-									} finally {
-										auline.drain();
-									}
-								} catch (final LineUnavailableException e) {
-									throw new WAVException(e);
-								}
-							} catch (final UnsupportedAudioFileException e) {
-								throw new WAVException(e);
-							} catch (final IOException e) {
-								throw new WAVException(e);
-							}
-						}
-					}.start();
-				} catch (final IOException e) {
-					throw new WAVException(e);
-				}
-			}
-		}
-	}
+    @Override
+    public void play() {
+        if (this.filename != null) {
+            final File soundFile = new File(this.filename);
+            if (soundFile.exists()) {
+                try (FileInputStream inputStream = new FileInputStream(
+                        soundFile)) {
+                    new Thread() {
+                        @Override
+                        public void run() {
+                            try (AudioInputStream audioInputStream = AudioSystem
+                                    .getAudioInputStream(inputStream)) {
+                                final AudioFormat format = audioInputStream
+                                        .getFormat();
+                                final DataLine.Info info = new DataLine.Info(
+                                        SourceDataLine.class, format);
+                                try (Line res = AudioSystem.getLine(info);
+                                        SourceDataLine line = (SourceDataLine) res) {
+                                    if (line != null) {
+                                        line.open(format);
+                                        line.start();
+                                        int nBytesRead = 0;
+                                        final byte[] abData = new byte[WAVPlayer.EXTERNAL_BUFFER_SIZE];
+                                        try {
+                                            while (nBytesRead != -1) {
+                                                nBytesRead = audioInputStream
+                                                        .read(abData, 0,
+                                                                abData.length);
+                                                if (nBytesRead >= 0) {
+                                                    line.write(abData, 0,
+                                                            nBytesRead);
+                                                }
+                                            }
+                                        } catch (final IOException e) {
+                                            throw new WAVException(e);
+                                        } finally {
+                                            line.drain();
+                                        }
+                                    }
+                                } catch (final LineUnavailableException e) {
+                                    throw new WAVException(e);
+                                }
+                            } catch (final UnsupportedAudioFileException e) {
+                                throw new WAVException(e);
+                            } catch (final IOException e) {
+                                throw new WAVException(e);
+                            }
+                        }
+                    }.start();
+                } catch (final IOException e) {
+                    throw new WAVException(e);
+                }
+            }
+        }
+    }
 }
